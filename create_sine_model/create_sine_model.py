332b5313a31562694aca544332a2e2cdd1556ec8
Ryan Davis <ryrutdavis@gmail.com>
2024-05-03 15:38:28 +0000

    first commit

---
+++ create_sine_model/create_sine_model.py
@@ -0,0 +1,48 @@
+# TensorFlow is an open source machine learning library
+import tensorflow as tf
+# Numpy is a math library
+import numpy as np
+import matplotlib
+# Matplotlib is a graphing library
+import matplotlib.pyplot as plt
+# math is Python's math library
+import math
+
+
+
+# We'll generate this many sample datapoints
+SAMPLES = 1000
+# Set a "seed" value, so we get the same random numbers each time we run this
+# notebook. Any number can be used here.
+SEED = 1337
+np.random.seed(SEED)
+tf.random.set_seed(SEED)
+# Generate a uniformly distributed set of random numbers in the range from
+# 0 to 2π, which covers a complete sine wave oscillation
+x_values = np.random.uniform(low=0, high=2*math.pi, size=SAMPLES)
+# Shuffle the values to guarantee they're not in order
+np.random.shuffle(x_values)
+# Calculate the corresponding sine values
+y_values = np.sin(x_values)
+# Add a small random number to each y value
+y_values += 0.1 * np.random.randn(*y_values.shape)
+
+
+# We'll use 60% of our data for training and 20% for testing. The remaining 20%
+# will be used for validation. Calculate the indices of each section.
+TRAIN_SPLIT = int(0.6 * SAMPLES)
+TEST_SPLIT = int(0.2 * SAMPLES + TRAIN_SPLIT)
+# Use np.split to chop our data into three parts.
+# The second argument to np.split is an array of indices where the data will be
+# split. We provide two indices, so the data will be divided into three chunks.
+
+x_train, x_validate, x_test = np.split(x_values, [TRAIN_SPLIT, TEST_SPLIT])
+y_train, y_validate, y_test = np.split(y_values, [TRAIN_SPLIT, TEST_SPLIT])
+# Double check that our splits add up correctly
+assert (x_train.size + x_validate.size + x_test.size) == SAMPLES
+Plot the data in each partition in different colors:
+plt.plot(x_train, y_train, 'b.', label="Train")
+plt.plot(x_validate, y_validate, 'y.', label="Validate")
+plt.plot(x_test, y_test, 'r.', label="Test")
+plt.legend()
+plt.show()
